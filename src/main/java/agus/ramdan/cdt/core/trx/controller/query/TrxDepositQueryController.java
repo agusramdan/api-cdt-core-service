3d54b758a955cb7134237d6bf34291000fea67c5
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-03-11 12:53:18 +0700

    Add feature limit untuk getAll

--- src/main/java/agus/ramdan/cdt/core/trx/controller/query/TrxDepositQueryController.java
+++ src/main/java/agus/ramdan/cdt/core/trx/controller/query/TrxDepositQueryController.java
@@ -1,8 +1,12 @@
 package agus.ramdan.cdt.core.trx.controller.query;
 
+import agus.ramdan.base.controller.BaseQueryController;
+import agus.ramdan.cdt.core.trx.controller.dto.TrxDepositDTO;
 import agus.ramdan.cdt.core.trx.controller.dto.deposit.TrxDepositQueryDTO;
+import agus.ramdan.cdt.core.trx.controller.dto.qrcode.QRCodeQueryDTO;
 import agus.ramdan.cdt.core.trx.service.deposit.TrxDepositQueryService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -19,16 +23,17 @@ import java.util.UUID;
 @Tag(name = "Cash Deposit API", description = "APIs for query Cash Deposit")
 @RequiredArgsConstructor
 @Log4j2
-public class TrxDepositQueryController {
+@Getter
+public class TrxDepositQueryController implements BaseQueryController<TrxDepositQueryDTO, String> {
     private final TrxDepositQueryService service;
 
-    @GetMapping
-    public ResponseEntity<List<TrxDepositQueryDTO>> getAll() {
-        return ResponseEntity.ok(service.getAllTrxDeposits());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<TrxDepositQueryDTO> getById(@PathVariable UUID id) {
-        return ResponseEntity.ok(service.getTrxDepositById(id));
-    }
+//    @GetMapping
+//    public ResponseEntity<List<TrxDepositQueryDTO>> getAll() {
+//        return ResponseEntity.ok(service.getAllTrxDeposits());
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<TrxDepositQueryDTO> getById(@PathVariable UUID id) {
+//        return ResponseEntity.ok(service.getTrxDepositById(id));
+//    }
 }
--- src/main/java/agus/ramdan/cdt/core/trx/service/deposit/TrxDepositQueryService.java
+++ src/main/java/agus/ramdan/cdt/core/trx/service/deposit/TrxDepositQueryService.java
@@ -1,9 +1,15 @@
 package agus.ramdan.cdt.core.trx.service.deposit;
 
+import agus.ramdan.base.service.BaseQueryEntityService;
 import agus.ramdan.cdt.core.trx.controller.dto.deposit.TrxDepositQueryDTO;
+import agus.ramdan.cdt.core.trx.controller.dto.qrcode.QRCodeQueryDTO;
 import agus.ramdan.cdt.core.trx.mapper.TrxDepositMapper;
+import agus.ramdan.cdt.core.trx.persistence.domain.QRCode;
+import agus.ramdan.cdt.core.trx.persistence.domain.TrxDeposit;
 import agus.ramdan.cdt.core.trx.persistence.repository.TrxDepositRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +17,10 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class TrxDepositQueryService {
+@Getter
+public class TrxDepositQueryService implements
+        BaseQueryEntityService<TrxDeposit, UUID, TrxDepositQueryDTO, String> {
+
     private final TrxDepositRepository repository;
     private final TrxDepositMapper queryMapper;
 
@@ -23,6 +32,21 @@ public class TrxDepositQueryService {
         return repository.findById(id)
                 .map(queryMapper::entityToQueryDto)
                 .orElseThrow(() -> new RuntimeException("Transaction not found"));
+    }
+
+    @Override
+    public UUID convertId(String string) {
+        return UUID.fromString(string);
+    }
+
+    @Override
+    public TrxDepositQueryDTO convertOne(TrxDeposit entity) {
+        return queryMapper.entityToQueryDto(entity);
+    }
+
+    @Override
+    public TrxDepositQueryDTO convert(TrxDeposit entity) {
+        return queryMapper.entityToQueryDto(entity);
     }
 }
 
