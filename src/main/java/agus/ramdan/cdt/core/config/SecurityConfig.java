0463ab740c585368747485cc8881ce86409a0a9b
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-03-16 14:45:30 +0700

    log config

--- src/main/java/agus/ramdan/cdt/core/config/SecurityConfig.java
+++ src/main/java/agus/ramdan/cdt/core/config/SecurityConfig.java
@@ -22,10 +22,27 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui.html","/swagger-ui/**", "/v3/api-docs/**","/v3/api-docs.yaml").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/cdt/cdm/**").hasAuthority("SCOPE_cdm.read")
-                        .requestMatchers(HttpMethod.POST,"/api/cdt/cdm/**").hasAuthority("SCOPE_cdm.write")
-                        .requestMatchers(HttpMethod.PUT,"/api/cdt/cdm/**").hasAuthority("SCOPE_cdm.write")
-                        .requestMatchers(HttpMethod.DELETE,"/api/cdt/cdm/**").hasAuthority("SCOPE_cdm.delete")
+
+                        .requestMatchers(HttpMethod.GET,"/api/cdt/core/master/**").hasAnyAuthority("SCOPE_web.internal.read","SCOPE_admin.internal.read")
+                        .requestMatchers(HttpMethod.POST,"/api/cdt/core/master/**").hasAnyAuthority("SCOPE_web.internal.create","SCOPE_admin.internal.create")
+                        .requestMatchers(HttpMethod.PUT,"/api/cdt/core/master/**").hasAnyAuthority("SCOPE_web.internal.update","SCOPE_admin.internal.update")
+                        .requestMatchers(HttpMethod.DELETE,"/api/cdt/core/master/**").hasAnyAuthority("SCOPE_web.internal.delete","SCOPE_admin.internal.delete")
+
+                        .requestMatchers(HttpMethod.GET,"/api/cdt/core/trx/qr-code/command").hasAnyAuthority("SCOPE_web.internal.read","SCOPE_cdm.read")
+                        .requestMatchers(HttpMethod.POST,"/api/cdt/core/trx/qr-code/command").hasAuthority("SCOPE_web.internal.create")
+                        .requestMatchers(HttpMethod.PUT,"/api/cdt/core/trx/qr-code/command").hasAuthority("SCOPE_web.internal.update")
+                        .requestMatchers(HttpMethod.DELETE,"/api/cdt/core/trx/qr-code/command").hasAuthority("SCOPE_web.internal.delete")
+
+                        .requestMatchers(HttpMethod.GET,"/api/cdt/core/trx/deposit/command").hasAuthority("SCOPE_cdm.read")
+                        .requestMatchers(HttpMethod.POST,"/api/cdt/core/trx/deposit/command").hasAuthority("SCOPE_cdm.write")
+                        .requestMatchers(HttpMethod.PUT,"/api/cdt/core/trx/deposit/command").hasAuthority("SCOPE_cdm.write")
+                        .requestMatchers(HttpMethod.DELETE,"/api/cdt/core/trx/deposit/command").hasAuthority("SCOPE_admin.internal.delete")
+
+                        .requestMatchers(HttpMethod.GET,"/api/cdt/core/trx/pickup/command").hasAuthority("SCOPE_cdm.read")
+                        .requestMatchers(HttpMethod.POST,"/api/cdt/core/trx/pickup/command").hasAuthority("SCOPE_cdm.write")
+                        .requestMatchers(HttpMethod.PUT,"/api/cdt/core/trx/pickup/command").hasAuthority("SCOPE_cdm.write")
+                        .requestMatchers(HttpMethod.DELETE,"/api/cdt/core/trx/pickup/command").hasAuthority("SCOPE_admin.internal.delete")
+
                         .anyRequest().authenticated()
             ).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
