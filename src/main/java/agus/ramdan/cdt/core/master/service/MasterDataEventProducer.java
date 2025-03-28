98a7c31d9663f3df74c1477b5bda786f892cf64c
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-03-28 19:52:20 +0700

    fix format Local data,time and datetime serializable kafka

--- src/main/java/agus/ramdan/cdt/core/master/service/MasterDataEventProducer.java
+++ src/main/java/agus/ramdan/cdt/core/master/service/MasterDataEventProducer.java
@@ -2,7 +2,6 @@ package agus.ramdan.cdt.core.master.service;
 
 import agus.ramdan.base.dto.DataEvent;
 import agus.ramdan.base.service.BaseCommandEntityService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,6 @@ public abstract class MasterDataEventProducer<T, ID, ResultDTO, CreateDTO, Updat
     private KafkaTemplate<String, DataEvent> kafkaTemplate;
     @Autowired
     private ObjectMapper objectMapper;
-
     public void publishDataEvent(DataEvent dataEvent) {
         try {
             byte[] object = objectMapper.writeValueAsBytes(dataEvent.getData());
